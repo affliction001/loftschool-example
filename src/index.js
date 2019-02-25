9ab2b55383557c436264a23624ba5ebff9135ba7
Vladislav Semenov <affliction001@mail.ru>
2019-02-25 20:30:05 +0300

    Шестое домашнее задание. Есть проблема со второй задачей.

--- src/index.js
+++ src/index.js
@@ -1,101 +1,52 @@
-/* ДЗ 5 - DOM Events */
+/* ДЗ 6 - Асинхронность и работа с сетью */
 
 /*
  Задание 1:
 
- Функция должна добавлять обработчик fn события eventName к элементу target
+ Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунду
 
  Пример:
-   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function addListener(eventName, target, fn) {
-    target.addEventListener(eventName, fn);
+function delayPromise(seconds) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, seconds * 1000);
+	});
 }
 
 /*
  Задание 2:
 
- Функция должна удалять у элемента target обработчик fn события eventName
+ 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
 
  Пример:
-   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
-function removeListener(eventName, target, fn) {
-    target.removeEventListener(eventName, fn);
-}
-
-/*
- Задание 3:
-
- Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
-
- Пример:
-   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
- */
-function skipDefault(eventName, target) {
-    target.addEventListener(eventName, event => {
-        event.preventDefault();
-    })
-}
-
-/*
- Задание 4:
-
- Функция должна эмулировать событие click для элемента target
-
- Пример:
-   emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
- */
-function emulateClick(target) {
-    const event = new MouseEvent('click', {
-        'view': window,
-        'bubbles': true,
-        'cancelable': true
-    });
-
-    target.dispatchEvent(event);
-}
-
-/*
- Задание 5:
-
- Функция должна добавить такой обработчик кликов к элементу target,
- который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
-
- Пример:
-   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
- */
-function delegate(target, fn) {
-    target.addEventListener('click', event => {
-        if (event.target.tagName === 'BUTTON') {
-            fn();
-        }
-    });
-}
-
-/*
- Задание 6:
-
- Функция должна добавить такой обработчик кликов к элементу target,
- который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
-
- Пример:
-   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
- */
-function once(target, fn) {
-    function oneTimeFunction() {
-        target.removeEventListener('click', oneTimeFunction);
-        fn();
-    }
-
-    target.addEventListener('click', oneTimeFunction);
+function loadAndSortTowns() {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		
+		xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+		xhr.responseType = 'json';
+		xhr.send();
+		xhr.addEventListener('load', () => {
+			if (xhr.status === 200 && xhr.readyState === 4) {
+				resolve(xhr.response.sort((o1, o2) => o1.name.localeCompare(o2.name)));
+			}
+			
+			reject();
+		});
+	});
 }
 
 export {
-    addListener,
-    removeListener,
-    skipDefault,
-    emulateClick,
-    delegate,
-    once
+    delayPromise,
+    loadAndSortTowns
 };
